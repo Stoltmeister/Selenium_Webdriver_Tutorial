9dd848d6b37fd601a5aa69c5bdbd039d05a9881f
Josh Stoltenberg <josh.stoltenberg@myhealthchampion.com>
2019-04-16 10:15:40 -0500

    Added WebdriverRadioButtons html and java file

---
+++ src/main/java/WebDriverRadioButtons.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class WebDriverRadioButtons {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///C:/WebDriverDemo/src/main/webapp/RadioButtonTest.html");
+
+        List<WebElement> radioButtons = driver.findElements(By.name("color"));
+        radioButtons.get(1).click();
+    }
+}
