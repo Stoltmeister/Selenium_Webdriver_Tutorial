9b05484a33e1fba2bb27e37bc35b59dd3e7ca3ef
Josh Stoltenberg <josh.stoltenberg@myhealthchampion.com>
2019-04-18 09:06:39 -0500

    selenium server seems to work correctly, added comments for setup in WebDriverDemo.java file

--- src/main/java/WebDriverDemo.java
+++ src/main/java/WebDriverDemo.java
@@ -5,17 +5,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class WebDriverDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        // Have to set system variables to avoid having to do this every 
         //System.setProperty("webdriver.gecko.driver", "C:/Tools/geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C:/Tools/chromedriver.exe");
         //System.setProperty("webdriver.edge.driver", "C:/Tools/MicrosoftWebDriver.exe");
 
+        // Selenium Server requires this file to be downloaded and run this command from the commandline
+        // in that file's directory like this: (java -jar "selenium-server-standalone-3.141.59.jar" -port 8090)
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8090/wd/hub"), DesiredCapabilities.chrome());
+
         //WebDriver driver = new FirefoxDriver();
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         //WebDriver driver = new EdgeDriver();
         //Can't test safari on windows
 
@@ -24,6 +36,10 @@ public class WebDriverDemo {
         WebElement searchField = driver.findElement(By.name("q"));
         searchField.sendKeys("pluralsight");
         searchField.submit();
+
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Images")));
 
         WebElement imagesLink = driver.findElements(By.linkText("Images")).get(0);
         imagesLink.click();
